de341a2cd13772aee36d0ee74ce62d5b661b0d57
Yu0972 <105021045@live.asia.edu.tw>
2016-11-14 16:04:38 +0800

    ex

---
+++ src/ex/ex02.java
@@ -0,0 +1,14 @@
+package ex;
+import java.util.Scanner;
+/*
+ * Date: 2016/11/14
+ * Author: 105021045 ³¢·¶§e
+ */
+public class ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/ex/ex04.java
+++ src/ex/ex04.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
  * Date: 2016/11/14
  * Author: 105021045 ³¢·¶§e
  */
-public class ex01 {
+public class ex04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner ( System.in ) ;
-		System.out.print ( "¿é¤J¤@­Ón=" ) ;
-		int n = scn.nextInt();
-		var ( n ) ;
+		int a = scn.nextInt () ;
+		System.out.println ( fun ( a ) ) ;
 }
-		public static void var ( int n ) {
-		Scanner scn = new Scanner ( System.in ) ;
-
+		public static int fun ( int b ) {
+			if ( b == 1 ) {
+				return b ;
+			}else{
+				return b * fun( b - 1 ) ;
+		}
 	}
 }
